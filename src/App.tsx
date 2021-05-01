cf45c45d7e9f2c87cfb9edd6632c8ed6ac4b246d
chardwillian <chardwillian9@gmail.com>
2021-04-30 22:36:54 -0300

    GobarberMobile

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+// import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    {/* Se apagar as duas próximas linhas o layout fica no centro certinho */}
+    {/* <StatusBar barStyle="light-content" backgroundColor="312e38" /> */}
+    <AppProvider>
+      {/*   <View style={{ flex: 1, backgroundColor: '#312e38' }} />  */}
+      <Routes />
+    </AppProvider>
+  </NavigationContainer>
+);
+
+export default App;
